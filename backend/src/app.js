2d15c74dacf39f132f7b65e6d78f8cb401cf3727
Mᴇɢʜ Pᴀᴛᴇʟ <meghpatel0009@gmail.com>
2026-08-25 21:45:04 +0530

    feat: implement project routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,33 @@ import express from 'express'
 export const app = express() // express application obj
 
 // * configuration
-app.get('/', (req, res) => { // '/' verifies the path and then executes the cb func , req and res are also obj
+app.get('/', (req, res) => {
+  // '/' verifies the path and then executes the cb func , req and res are also obj
   res.send('hello world')
+})
+
+const data = {
+  1: { name: 'Project1', id: 1, type: 'software' },
+  2: { name: 'Project2', id: 2, type: 'hardware' },
+  3: { name: 'Project3', id: 3, type: 'software' },
+}
+const dataStats = {
+  1: { workDone: '10%', id: 1, type: 'software' },
+  2: { workDone: '30%', id: 2, type: 'hardware' },
+  3: { workDone: '100%', id: 3, type: 'software' },
+}
+// ! IMP : Route Order
+app.get('/projects', (req, res) => {
+  res.send(data)
+})
+
+app.get('/projects/stats', (req, res) => {
+  res.send(dataStats)
+})
+
+// * routes of ids or any parameter route should be at last
+app.get('/projects/:id', (req, res) => {
+  const id = req.params.id
+  if (Object.hasOwn(data, id)) res.send(data[id])
+  else res.status(404).json({ error: 'Id does not exists' })
 })
